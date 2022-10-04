0d9d9191c91a5a83edb75bf1b193f11288cabb9e
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2022-10-03 21:53:58 -0300

    feat: stage 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppComponent } from './app.component'
 import { InicioComponent } from './components/inicio/inicio.component'
 import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component'
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, InicioComponent, ArticulosFamiliasComponent],
+  declarations: [AppComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosFamilias', component: ArticulosFamiliasComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: "/" }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
